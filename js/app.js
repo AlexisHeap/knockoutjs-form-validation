f21efab1b4d7ef616f2ac8ed262941484d6f8b3b
Alexis Ibarra <alexis.ibarra@heapstash.com>
2023-03-07 10:33:00 -0600

    using a handleSubmit event handler

--- js/app.js
+++ js/app.js
@@ -6,6 +6,15 @@ function createAccountViewModel() {
     self.firstName.subscribe(function (newValue) {
         console.log('new value', newValue)
     })
+
+    self.handleSubmit = function () {
+        console.log('SUBMIT THE FORM')
+        var payload = {
+            firstName: self.firstName()
+        }
+        console.log(payload) 
+        // when i clicked the continue button it must send a response with firstNam0e
+    }
 };
 
 const knockoutApp = document.querySelector("#knockout-app");
