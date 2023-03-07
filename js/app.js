dc93bfbbd96811bdf19d5c29e27e608ca2832683
Alexis Ibarra <alexis.ibarra@heapstash.com>
2023-03-07 08:47:09 -0600

    new observable feature called subscribe where there're new values in the Debug console

--- js/app.js
+++ js/app.js
@@ -2,6 +2,10 @@ function createAccountViewModel() {
     var self = this;
 
     self.firstName = ko.observable("Alexis");
+
+    self.firstName.subscribe(function (newValue) {
+        console.log('new value', newValue)
+    })
 };
 
 const knockoutApp = document.querySelector("#knockout-app");
